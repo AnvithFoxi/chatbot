bd43b3cc998c7ae4be59be092d44ea90e24f1cfb
Venkata Anvith Raju <anvithraju007@gmail.com>
2026-06-26 10:21:20 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="anvith",
+    author_email="anvith0077@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
